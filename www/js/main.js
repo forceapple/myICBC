44233f1550bd8d7a6c14d4eaf28349038e83058b
forceapple <life147@live.com>
2014-12-08 13:40:56 -0800

    added camera function--- test1

--- www/js/main.js
+++ www/js/main.js
@@ -197,7 +197,7 @@ var page={
 						//Camera page			camera page functions ---------------
 
 												if(page.num ==2){
-
+													camera.getPicture()
 													page.go(page.image)
 													//put camera stuff here************
 													//*****temp to go next page
